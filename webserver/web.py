ef52c7d39763d3da3520bb11ba0ada8fc519dac2
strin <stl501@gmail.com>
2014-03-29 09:48:45 -0400

    some message

--- webserver/web.py
+++ webserver/web.py
@@ -17,9 +17,13 @@ def cook():
 	ings = ings_raw.split('_')
 	"create response recipes"
 	response = query.query(query.set_up(client), ings)
-	[recipes, links, scores] = zip(*response)
+	print 'response = ', response
+	if response == []:
+		recipes = links = scores = []
+	else: 
+		[recipes, links, scores] = zip(*response)
 	return jsonify(recipes=recipes, links=links, scores=scores)
-	
+
 if __name__ == "__main__":
 	client = query.connect()
 	app.run(debug = True)
