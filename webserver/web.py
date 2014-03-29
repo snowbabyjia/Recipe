10365a51a37b6bf6f157edeebd9f946402aa54bb
strin <stl501@gmail.com>
2014-03-29 03:52:11 -0400

    ui

--- webserver/web.py
+++ webserver/web.py
@@ -1,1 +1,12 @@
+from flask import *
+import interface as iface
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+	return render_template("index.html", ings=iface.get_ingredients_all())
+
+if __name__ == "__main__":
+	app.run(debug = True)
 
