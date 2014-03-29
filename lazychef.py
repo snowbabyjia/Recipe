687ff83d893fd2ba687bb9a44eef5dc791998fab
Giuseppe Coviello <gicoviello@gmail.com>
2014-03-29 16:24:34 -0400

    included user preferences

--- lazychef.py
+++ lazychef.py
@@ -14,6 +14,7 @@ LOCK = None
 MONGO_HOME = '/Users/gicoviello/mongodb-osx-x86_64-2.4.9'
 
 URLS = (
+        '/user_recipes/(.*)/(.*)', 'user_recipes',
         '/recipes/(.*)', 'recipes',
         '/rating/(.*)/(.*)', 'rating'
 )
@@ -77,6 +78,79 @@ class recipes:
 		# TODO: sort the recommandation
 		return sorted(output, key=output.get, reverse=True)
 
+def get_rating(user, recipe, lock, ratings, recommendations):
+	rows = ratings.find({'user' : user, 'recipe' : recipe})
+	if(rows.count() > 0):
+		return rows[0]['rating']
+
+	lock.acquire()
+	rows = recommendations.find({'user' : user, 'recipe' : recipe})
+	rating = 3
+	if(rows.count() > 0):
+		rating = rows[0]['rating']
+	lock.release()
+
+	return rating
+
+
+class user_recipes:
+	def GET(self, user, ingredients):
+
+		ingredients = ingredients.split(',')
+		recipe_has_ing = {}
+		recipe_require_ing = {}
+		recommandation = {}
+		output = {}
+		# For every ingredient user has, get all the recipes that uses the ingredient.
+		#
+		for ing in ingredients:
+			rows = web.ctx.RECIPES.find({"ingredient":ing})
+			print(rows.count())
+			#print rows.count()
+			# For every row that the ingredient is in, put the ingredient in the recipe entry in recipe_list
+			for i in range(rows.count()):
+				row = rows[i]
+				# key is the recipe_name
+				key = row["recipe_name"]
+				if not recipe_has_ing.has_key(key):
+					recipe_has_ing[key] = []
+				recipe_has_ing[key].append(ing)
+				# Add this recipe to potential recommanded recipes. initial rating is 0
+				if not recommandation.has_key(key):
+					recommandation[key] = 0.0
+
+				# Add the recipe and its required ingredients to the dict
+				if not recipe_require_ing.has_key(key):
+					# the value is a cursor object
+					recipe_require_ing[key] = web.ctx.RECIPES.find({"recipe_name": key})
+		# Update the ratingredients based on if an optional/required ing is present for each candidate recipe
+		for candidate in recommandation.keys():
+			total_weight = 0 # this is the total weight of ingredients of this recipe. Used for normalize
+			cursor = recipe_require_ing[candidate]
+			candidate_rating = 0
+			candidate_url = ""
+			for i in range(cursor.count()):
+				#each req_ing is a row in db that this candidate recipe is related
+				req_ing = cursor[i]
+				candidate_rating = get_rating(user, req_ing['recipe_name'], web.ctx.LOCK, web.ctx.RATINGS, web.ctx.RECOMMENDATIONS)
+				candidate_url = req_ing["url"]
+				total_weight += req_ing["weight"]
+				if req_ing["ingredient"] in recipe_has_ing[candidate]:
+					recommandation[candidate] += req_ing["weight"]
+				# if user does not have a required ing, delete this candidate.
+				elif req_ing["req"]:
+					del recommandation[candidate]
+					break
+				else:
+					continue
+			# If candidate is still in recommandation list, compute the rating
+			if candidate in recommandation:
+				recommandation[candidate] *= 1.0*candidate_rating/total_weight
+				recommandation[candidate] = int(recommandation[candidate] * 100) / 100.0
+				output[(candidate, candidate_url, recommandation[candidate])] = recommandation[candidate]
+
+		# TODO: sort the recommandation
+		return sorted(output, key=output.get, reverse=True)
 
 class rating:
 	def GET(self, user, recipe):
